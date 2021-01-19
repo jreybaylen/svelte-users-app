09845951b6d6f75ccb0124f2450ce479a7ed5a0e
John Rey <john.baylen@wondersco.com>
2021-01-19 00:51:38 -0500

    A simple app built with Svelte

---
+++ src/modules/Root.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Header from '@components/Header.svelte'
+    import Footer from '@components/Footer.svelte'
+    import Content from '@components/Content.svelte'
+    import HomePage from '@modules/pages/Home/index.svelte'
+</script>
+
+<Header />
+<Content className="container">
+    <HomePage />
+</Content>
+<Footer />
